9f579400afc88ff5006d22a084ed06f2ffcaba45
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2023-06-27 22:58:35 +0200

    Welcome contract updated

--- 00_welcome/Exercise.sol
+++ 00_welcome/Exercise.sol
@@ -4,5 +4,8 @@ pragma solidity 0.8.16;
 import "forge-std/Script.sol";
 
 contract Exercise {
-    function _remove(uint256[] storage _arr, uint256 _index) internal {}
+    function _remove(uint256[] storage _arr, uint256 _index) internal {
+        _arr[_index] = _arr[_arr.length - 1];
+        _arr.pop();
+    }
 }
