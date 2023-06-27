f24801112e635f11e79e4f207295626f5f29c004
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2023-06-27 23:58:14 +0200

    Delegation contract updated

--- 01_delegation/Exercise.sol
+++ 01_delegation/Exercise.sol
@@ -3,6 +3,22 @@ pragma solidity 0.8.16;
 
 import "forge-std/Script.sol";
 
-contract Exercise {
-    function setCounter(address _counter) external {}
+import "./ICounter.constant.sol";
+
+contract Exercise is ICounter {
+    ICounter private counterImplementation;
+
+    function setCounter(address _counter) external {
+        counterImplementation = ICounter(_counter);
+    }
+
+    function inc() override external {
+        require(address(counterImplementation) != address(0), "Counter implementation address not set");
+        counterImplementation.inc();
+    }
+
+    function counter() override external returns (uint256) {
+        require(address(counterImplementation) != address(0), "Counter implementation address not set");
+        return counterImplementation.counter();
+    }
 }
